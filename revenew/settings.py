7ee839e6275f64b4c5d522e13f80a84ade17a237
Felipe YZ <felipe.yan13@gmail.com>
2025-07-07 20:08:10 -0400

    Añadir CSRF_TRUSTED_ORIGINS a settings

--- revenew/settings.py
+++ revenew/settings.py
@@ -33,7 +33,10 @@ else:
 DEBUG = os.environ.get("DEBUG", "False").lower() in ("true", "1", "yes")
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
+if DEBUG:
+    CSRF_TRUSTED_ORIGINS = []
 # Application definition
 
 INSTALLED_APPS = [
